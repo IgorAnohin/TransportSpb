b88b0f9083ac5eeb6771422fd9472199c0a9d530
Igor Anokhin <igr.anokhin@gmail.com>
2020-09-06 23:00:53 +0300

    add API for transport ways

--- python_server/server.py
+++ python_server/server.py
@@ -1,8 +1,13 @@
+import json
 import io
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
+
 import urllib.request
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def hello():
@@ -23,6 +28,19 @@ def map():
     response = urllib.request.urlopen(url)
     data = response.read()
     return send_file(io.BytesIO(data), mimetype='image/png')
+
+@app.route("/way")
+def way():
+    url_template = 'http://transport.orgp.spb.ru/Portal/transport/map/stage?ROUTE={route_id}&SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&SRS=EPSG%3A900913&BBOX=3248267.954007,8290981.138665,3499755.331694,8460805.106258'
+    route_id = request.args["route_id"]
+
+    url = url_template.format(route_id=route_id)
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+    coordinates = []
+    for feature in data["features"]:
+        coordinates += feature["geometry"]["coordinates"]
+    return jsonify(coordinates)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
